89dfaa3952d5df7752db12071d2b5bf0824fef1e
jai-git4208 <jaiminpansal@gmail.com>
2025-11-26 02:42:56 +0530

    api change

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-import flask
+from flask import Flask, render_template
+import os
 
-app = flask.Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    """Serve the main index.html"""
+    return render_template('index.html')
+
+@app.route('/<path:path>')
+def serve_static(path):
+    """Serve any static files"""
+    return app.send_static_file(path)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4000, debug=True)
+    print("="*70)
+    print("MARKET MOVEMENT CLASSIFIER - FRONTEND SERVER")
+    print("="*70)
+    print("Frontend running on: http://localhost:8080")
+    print("Make sure backend API is running on: http://localhost:5000")
+    print("="*70 + "\n")
+    
+    # Try to open browser automatically
+    import webbrowser
+    try:
+        webbrowser.open('http://localhost:8080')
+    except:
+        pass
+    
+    app.run(debug=True, host='0.0.0.0', port=8080)
